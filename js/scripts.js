3c2ebe3c4554c94bfcd26eb7a7f67af5773288b8
yangzongyao <andy831201@gmail.com>
2024-02-26 00:31:32 +0800

    fix: fix multiple issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 /*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
+* ZY profile - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 ZY profile
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// $.ajax({
+//     url: "https://gitlab.com/users/andy831201/calendar.json",
+//     success: function(result){
+//         $("#gitlab")
+//     }
+// });
+
